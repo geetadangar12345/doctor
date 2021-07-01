550c5cb92f6cbec30ddab4f0a1133a82d73fd9cd
geeta <geeta@complitech.net>
2021-07-01 12:57:30 +0530

    Exercise1

---
+++ ruby_code.rb
@@ -0,0 +1,9 @@
+puts "enter your salary"
+num = gets.chomp.to_i
+number = num
+a = ["January", "February","March","April","May", "June", "July", "September", "October", "November", "December"]
+   loop do
+      b = a.each do |a|
+         puts a,num += number
+      end
+end
